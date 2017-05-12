7f75e2ac1e7d05879e49e2386c9894fb3651c3d3
SzuWei Lin <szuweilin@google.com>
2017-05-12 14:26:39 +0800

    athene: Move permission XMLs to vendor
    Move all permission XML files form:
    
      system/etc/permissions/*
    
    to
    
      vendor/etc/permissions/*
    
    Bug: 37927875
    Test: check these files in folder vendor/etc/permissions
    Change-Id: Iaebed412a99c26bb89f7024618e3c2c4bf63ca4b

--- device.mk
+++ device.mk
@@ -63,8 +63,8 @@ PRODUCT_COPY_FILES += \
 
 # Motorola Camera permissions
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/com.motorola.camera.xml:system/etc/permissions/com.motorola.camera.xml \
-    $(LOCAL_PATH)/configs/com.motorola.cameraone.xml:system/etc/permissions/com.motorola.cameraone.xml
+    $(LOCAL_PATH)/configs/com.motorola.camera.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.motorola.camera.xml \
+    $(LOCAL_PATH)/configs/com.motorola.cameraone.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.motorola.cameraone.xml
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
@@ -326,7 +326,7 @@ PRODUCT_PACKAGES += \
     rild_socket
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
