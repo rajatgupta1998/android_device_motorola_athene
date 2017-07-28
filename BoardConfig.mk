3ef559dde4c9536105f1dcd5585a9efcf88db955
Rajat Gupta <rjtgupta84@gmail.com>
2017-07-28 09:07:44 +0530

    Remove dtbTool_moto

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,7 +52,6 @@ BOARD_KERNEL_SEPARATED_DT := true
 TARGET_CUSTOM_DTBTOOL := dtbTool_custom
 BOARD_DTBTOOL_ARGS := --force-v3 --motorola 1
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-TARGET_CUSTOM_DTBTOOL := dtbTool_moto
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_CONFIG := athene_defconfig
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8952
