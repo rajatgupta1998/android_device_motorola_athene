424f180c51f9beed34a60b4eb26731e0b5c91081
Rajat Gupta <rjtgupta84@gmail.com>
2017-08-04 16:35:04 +0530

    Set default Brightness to 100  *Default value is too much for the device.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -157,3 +157,4 @@ TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_THEME := portrait_hdpi
 TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_DEFAULT_BRIGHTNESS := 100
