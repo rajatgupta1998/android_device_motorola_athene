1b0f52e1ec403e50b2bbed9cea40f12ab163dda2
Vachounet <vachounet@live.fr>
2017-08-25 15:57:42 +0000

    athene :  build Vibrator HAL
    Change-Id: Ifa32ab9be992e0eaa42080cdb299b9cfaed7eac2

--- device.mk
+++ device.mk
@@ -264,6 +264,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-athene.conf:system/etc/thermal-engine-athene.conf
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl
+
 # Wifi
 PRODUCT_PACKAGES += \
     hostapd \
