84fd8239e3806fe0972336cca784f73f6be81559
Nikit Sharma <nikitsharma92@gmail.com>
2017-09-09 02:35:55 +0530

    athene: Build camera HAL
    Change-Id: I18073a4e13ef8cf4ffa3b081f89830818282b1f5

--- device.mk
+++ device.mk
@@ -103,6 +103,8 @@ PRODUCT_COPY_FILES +=  \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@3.2-impl \
     camera.msm8952 \
     libbson \
     Snap
