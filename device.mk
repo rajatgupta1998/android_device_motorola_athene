d3b9f4245ee444ea465b5362d3838d9006d25162
Nikit Sharma <nikitsharma92@gmail.com>
2017-09-09 02:50:10 +0530

    athene : build Keymaster HAL
    Change-Id: I8273f919c4a6d20c86245ca48145352dbd6eee53

--- device.mk
+++ device.mk
@@ -161,6 +161,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     keystore.msm8952
 
+# Keymaster HAL
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl
+ 
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
