37ca83d94eb185019c4f0edbfe58fac88f74e3b7
LuK1337 <priv.luk@gmail.com>
2017-09-08 13:46:20 +0200

    athene: add basic 2.0 -> 2.1 fingerprint HAL wrapper
    Change-Id: I27963bfb1402aba10e4ceba669520b630d075abf

--- device.mk
+++ device.mk
@@ -142,7 +142,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1-service \
+    fingerprint.msm8952
 
 # FM
 PRODUCT_PACKAGES += \
