b29dbaaa7d542128df7949a84323a283571e6f46
Ethan Chen <intervigil@gmail.com>
2017-11-22 20:55:53 +0100

    athene: add AIDs for legacy RFS
    Change-Id: I7cdaa7eac3de0f0782f44ec5be5ed75211c36bf0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,7 @@ LOCAL_PATH := device/motorola/athene
 BOARD_VENDOR := motorola-qcom
 
 # AIDs and CAPS
+TARGET_ALLOW_LEGACY_AIDS := true
 TARGET_FS_CONFIG_GEN := \
     $(LOCAL_PATH)/fs_config/mot_aids.txt \
     $(LOCAL_PATH)/fs_config/file_caps.txt
