8d72f9d50a5f1fd7cbdbc7096082e86375219907
Nikit Sharma <nikitsharma92@gmail.com>
2018-01-05 15:36:34 +0530

    Audio: Convert audio_effects.conf to xml
    * Based on https://android.googlesource.com/device/google/taimen/+/ecf82951a431324bd129b31495043dcfa00cfb1f
    * Additionally to the same tests done by Google, it was verified that with this change "XML effect configuration loaded successfully.", while before it showed: "XML effect configuration failed to load." .
    
    Conflicts:
    	device.mk

--- device.mk
+++ device.mk
@@ -93,10 +93,10 @@ PRODUCT_PACKAGES += \
     audio.primary.msm8952
 
 PRODUCT_COPY_FILES +=  \
-    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/audio/audio_effects.xml:system/vendor/etc/audio_effects.xml \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/vendor/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/vendor/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/vendor/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio/ap_gain.bin:system/etc/ap_gain.bin \
     $(LOCAL_PATH)/audio/ap_gain_mmul.bin:system/etc/ap_gain_mmul.bin \
