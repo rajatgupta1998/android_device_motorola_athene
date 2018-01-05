17b1d53303cee365de4ca3738e1d6ed78cdf7e26
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-05 18:18:12 +0100

    athene: Build libdisplayconfig and libhwc2on1adapter

--- device.mk
+++ device.mk
@@ -131,6 +131,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.graphics.composer@2.1-impl \
+    libdisplayconfig \
+    libhwc2on1adapter \
     gralloc.msm8952 \
     copybit.msm8952 \
     hwcomposer.msm8952 \
