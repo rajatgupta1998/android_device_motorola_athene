15cb1d5bc242da6ee897f1e27ca4eeaf9724295a
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-01-05 13:31:25 -0200

    athene: Build a 32 bit version of rild and fp service
    For now we're using 32 bits from stock, for both services. So use it until 64 bits blobs are ready
    
    Signed-off-by: jhenrique09 <jhenrique09.mcz@hotmail.com>

--- device.mk
+++ device.mk
@@ -155,7 +155,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service \
+    android.hardware.biometrics.fingerprint@2.1-service_32 \
     fingerprint.msm8952
 
 # Gatekeeper HAL
@@ -321,10 +321,14 @@ PRODUCT_PACKAGES += \
     libcnefeatureconfig \
     librmnetctl \
     libshim_ril \
-    libxml2
+    libxml2 \
+    rild_32
 
 PRODUCT_PACKAGES += \
     rild_socket
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rild/rild.rc:system/vendor/etc/init/rild.rc
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
