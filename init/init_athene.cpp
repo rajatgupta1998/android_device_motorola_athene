98970a23b7f54bf3bc886be75247e30da35af5db
Frostbite <burakdede.frostbite@gmail.com>
2018-01-27 11:57:37 +0300

    [WIP|DNM] init: Use Pixel's fingerprint to bypass SafetyNet Can't bypass SafetyNet with Moto G4/G4 Plus fingerprint currently
    Signed-off-by: Rajat Gupta <rjtgupta84@gmail.com>

--- init/init_athene.cpp
+++ init/init_athene.cpp
@@ -83,14 +83,14 @@ void vendor_load_properties()
         /* Moto G4 (XT162x) */
         property_override("ro.product.device", "athene");
         property_override("ro.build.description", "athene-user 7.0 NPJS25.93-14-13 3 release-keys");
-        property_override("ro.build.fingerprint", "motorola/athene/athene:7.0/NPJS25.93-14-13/3:user/release-keys");
+        property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
         property_override("ro.product.model", "Moto G4");
         property_set("ro.telephony.default_network", "10");
     } else {
         /* Moto G4 Plus (XT164x) */
         property_override("ro.product.device", "athene_f");
         property_override("ro.build.description", "athene_f-user 7.0 NPJS25.93-14-13 3 release-keys");
-        property_override("ro.build.fingerprint", "motorola/athene_f/athene_f:7.0/NPJS25.93-14-13/3:user/release-keys");
+        property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
         property_override("ro.product.model", "Moto G4 Plus");
         property_set("ro.telephony.default_network", "10,0");
     }
