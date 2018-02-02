d30b8fd394ec3bca9a7c08096385fcd1aee18ef7
Nikit Sharma <nikitsharma92@gmail.com>
2018-02-02 15:13:42 +0530

    athene: gps: Import from LA.BR.1.3.7-03510-8976.0 release

--- device.mk
+++ device.mk
@@ -165,18 +165,15 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl \
-    gps.msm8952 \
-    libshims_get_process_name \
-    libgnsspps
+    libgps.utils \
+    gps.default \
+    libgnss \
+    liblocation_api \
+    android.hardware.gnss@1.0-impl-qti \
+    libloc_api-rpc-qc
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/etc/lowi.conf:system/etc/lowi.conf \
-    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
-    $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf
+    $(LOCAL_PATH)/gps/etc/gps.conf:system/vendor/etc/gps.conf
 
 # IRSC
 PRODUCT_COPY_FILES += \
