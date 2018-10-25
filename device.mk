85b2e7bf9a42f6e70da560ab25d7ff0d77a4affb
LuK1337 <priv.luk@gmail.com>
2018-10-25 19:48:06 +0200

    athene: Drop libbt-vendor
    * With QTI bluetooth it's no longer needed.
    
    Change-Id: Iba4b0b6ba24fe1b85e7175291ed84769615ffadd

--- device.mk
+++ device.mk
@@ -202,10 +202,6 @@ PRODUCT_PACKAGES += \
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
- 
-# Bluetooth
-PRODUCT_PACKAGES += \
-    libbt-vendor 
 
 # Memtrack HAL
 PRODUCT_PACKAGES += \
