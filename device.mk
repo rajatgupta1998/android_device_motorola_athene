4162cac47e89871b26ad5f0d9e44dc09c33af36d
Lyudmila17 <merkel.naller@gmail.com>
2018-11-01 20:01:01 +0100

    athene: Ship missing GPS config
    Change-Id: Ia558a2c8eb7d08dac8b40ca8d09aaf571e95a88f

--- device.mk
+++ device.mk
@@ -193,7 +193,10 @@ PRODUCT_PACKAGES += \
     libloc_api-rpc-qc
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/etc/gps.conf:system/vendor/etc/gps.conf
+    $(LOCAL_PATH)/gps/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
+    $(LOCAL_PATH)/gps/etc/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
+    $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
 # IRSC
 PRODUCT_COPY_FILES += \
--- gps/etc/Android.mk
+++ gps/etc/Android.mk
@@ -1,12 +1,34 @@
 
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
 
+include $(CLEAR_VARS)
 LOCAL_MODULE := gps.conf
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/etc/
 LOCAL_SRC_FILES := gps.conf
-
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := izat.conf
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/etc/
+LOCAL_SRC_FILES := izat.conf
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := lowi.conf
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/etc/
+LOCAL_SRC_FILES := lowi.conf
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := xtwifi.conf
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/etc/
+LOCAL_SRC_FILES := xtwifi.conf
+include $(BUILD_PREBUILT)
