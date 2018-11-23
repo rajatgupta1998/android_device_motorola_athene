4b82dc9eb6a74d9110c602dfa2699e2e15036eaf
Rajat Gupta <rjtgupta84@gmail.com>
2018-11-23 12:44:12 +0530

    athene: board: no need to explicitly specify kernel toolchain

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -63,8 +63,6 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CONFIG := athene_defconfig
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8952
-KERNEL_TOOLCHAIN := ./prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-androidkernel-
 
 # Audio
 AUDIO_FEATURE_ENABLED_AAC_ADTS_OFFLOAD := true
