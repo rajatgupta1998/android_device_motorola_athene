5b3b40601c7b90a9d042657bab2261d641d0cbdd
Rajat Gupta <rajatgupta1998@acm.org>
2019-08-06 18:32:21 +0530

    athene: board: fix path for ims shim
    Signed-off-by: Rajat Gupta <rajatgupta1998@acm.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -139,7 +139,7 @@ TARGET_QCOM_NO_FM_FIRMWARE := true
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
-    /system/vendor/lib64/lib-imsvt.so|libshims_ims.so \
+    /system/vendor/lib/lib-imsvt.so|libshims_ims.so \
     /system/vendor/lib/libizat_core.so|libshims_get_process_name.so \
     /system/vendor/lib/libril-qc-qmi-1.so|rild_socket.so \
     /system/lib/libandroid.so|libshim_ril.so \
