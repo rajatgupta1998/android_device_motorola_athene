e8031e9477582f110121ec4899db365cd4af7976
Rajat Gupta <rajatgupta1998@acm.org>
2019-11-22 21:35:40 +0530

    Use Toolbox

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,3 +80,4 @@ TARGET_USERIMAGES_USE_F2FS := true
 RECOVERY_SDCARD_ON_DATA := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_THEME := portrait_hdpi
+TW_USE_TOOLBOX := true
