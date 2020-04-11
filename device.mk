6215681e6317c369f3374bfa3c6693bd840a879c
Rajat Gupta <rjtgupta84@gmail.com>
2020-04-11 23:23:52 +0530

    athene: Binderize bluetooth hal
    Signed-off-by: Rajat Gupta <rjtgupta84@gmail.com>

--- device.mk
+++ device.mk
@@ -212,7 +212,8 @@ PRODUCT_PACKAGES += \
  
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service
 
 # Memtrack HAL
 PRODUCT_PACKAGES += \
