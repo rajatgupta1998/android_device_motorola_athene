8605f0094b0c42cca069289e5e3d9cca39fff24b
Rajat Gupta <rjtgupta84@gmail.com>
2020-04-11 16:17:00 +0530

    athene: Bluetooth: Use libbt-vendor from stock O 8.1
    Signed-off-by: Rajat Gupta <rjtgupta84@gmail.com>

--- device.mk
+++ device.mk
@@ -212,8 +212,7 @@ PRODUCT_PACKAGES += \
  
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
-    libbt-vendor 
+    android.hardware.bluetooth@1.0-impl
 
 # Memtrack HAL
 PRODUCT_PACKAGES += \
